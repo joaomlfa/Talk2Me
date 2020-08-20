28ff661cc8838f9d09e2738df80bd25790b1cdbb
joaomlfa <280493Jf@>
2020-08-20 14:49:49 -0300

    Primeiro Commit

---
+++ app/src/main/java/com/example/talk2me/Activity/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.example.talk2me.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.talk2me.MainActivity;
+import com.example.talk2me.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                abrirApp();
+            }
+        },4000);
+
+    }
+    private void abrirApp(){
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
